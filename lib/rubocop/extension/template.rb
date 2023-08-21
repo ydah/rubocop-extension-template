3862c3dec9552d7b4dca92b092dbc798a02bade1
ydah <13041216+ydah@users.noreply.github.com>
2023-08-21 13:52:49 +0900

    Initial commit

---
+++ lib/rubocop/extension/template.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "template/version"
+
+module Rubocop
+  module Extension
+    module Template
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
